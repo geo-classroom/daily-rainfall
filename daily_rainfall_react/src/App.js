3cc97a5735f3d218f8e11299d935990e7bdbefa7
Riley <riley.kamstra@gmail.com>
2022-05-29 16:39:30 +0200

    Map componenet added

--- daily_rainfall_react/src/App.js
+++ daily_rainfall_react/src/App.js
@@ -3,12 +3,14 @@ import "./styles.css"
 
 import Navbar from "./components/Navbar" 
 import DateFilter from "./components/DateFilter"
+import Map from "./components/Map"
 
 function App() {
     return (
         <div>
             <Navbar/>
             <DateFilter/>
+            <Map/>
         </div>
     )
 }
--- daily_rainfall_react/src/components/Map.js
+++ daily_rainfall_react/src/components/Map.js
@@ -6,7 +6,7 @@ import "leaflet/dist/leaflet.css"
 
 function Map() {
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13}>
+        <MapContainer center={[-28.7, 24]} zoom={6}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
