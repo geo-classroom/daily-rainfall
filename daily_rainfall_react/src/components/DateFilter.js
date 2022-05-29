e934ef441e1bbf19fee28679f912c1a4df10190e
Riley <riley.kamstra@gmail.com>
2022-05-29 17:53:45 +0200

    DateFilter added

---
+++ daily_rainfall_react/src/components/DateFilter.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function DateFilter() {
+    return (
+        <div id="datefilter-container">
+            <button>Start Date</button>
+            <button>End Date</button>
+        </div>
+    )
+}
+
+export default DateFilter
