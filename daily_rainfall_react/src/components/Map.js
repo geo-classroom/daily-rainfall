ae98b00469e321869806572790a6c04b235ecf5a
Riley <riley.kamstra@gmail.com>
2022-05-30 11:04:24 +0200

    Added MapBox Streets as a basemap

--- daily_rainfall_react/src/components/Map.js
+++ daily_rainfall_react/src/components/Map.js
@@ -7,11 +7,18 @@ import "leaflet/dist/leaflet.css"
 function Map() {
     return (
         <MapContainer center={[-28.7, 24.5]} zoom={6}>
+            {/* Add a layer conroll to the to right of the map */}
             <LayersControl position="topright">
-            <LayersControl.BaseLayer name="Street" checked="true">
+                {/* 
+                    Add the following basemaps to the layer controller:
+                        MapBox Streets
+                        Stamen Terrain
+                        MapTiler Satellite 
+                */}
+                <LayersControl.BaseLayer name="Street" checked="true">
                     <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+                        url="https://api.mapbox.com/styles/v1/riley-5/cl3shshxv000515qntejbm29o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmlsZXktNSIsImEiOiJjbDNyZzdxeTIwbTAwM2NwZnN1cG41MWkxIn0.0EmF55wuBJY-2FHaRK73kQ"
                     />
                 </LayersControl.BaseLayer>
                 <LayersControl.BaseLayer name="Terrain">
