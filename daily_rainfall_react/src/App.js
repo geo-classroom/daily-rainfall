2fc4929d9ad4de369956246e6286b4ad76d43146
Riley <riley.kamstra@gmail.com>
2022-06-01 15:47:12 +0200

    Logout button implemented

--- daily_rainfall_react/src/App.js
+++ daily_rainfall_react/src/App.js
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import "./styles.css"
 // Firebase 
 import { initializeApp } from "firebase/app"
 import { config } from "./config/config"
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth"
+import { getDatabase, set, ref } from "firebase/database"
 // components
 import Navbar from "./components/Navbar" 
 import DateFilter from "./components/DateFilter"
@@ -11,24 +12,50 @@ import Map from "./components/Map"
 
 // Initialize the Firebase app
 initializeApp(config.firebaseConfig)
+const auth = getAuth()
 
 function App() {
-    const auth = getAuth()
+    const [currentUser, setCurrentUser] = useState("")
+
+    // Sign the user in with Google gmail account
     function signInWithGoogle() {
         signInWithPopup(auth, new GoogleAuthProvider())
         .then(result => {
-            console.log(result.user.uid)
+            // Calls function writeUserData
+            WriteUserData(result.user)
+            // Set the the currentUser to the signed in user 
+            setCurrentUser(auth.currentUser.displayName)
         })
         .catch((error) => {
             console.log(error)
         })
     }
 
+    // Saves the data from the signed in user to the database
+    function WriteUserData(user) {
+        const db = getDatabase()
+        set(ref(db, `users/${user.uid}`), {
+            username: user.displayName,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            isRegistered: false
+        })
+    }
+
+    function logout() {
+        signOut(auth).then(() => {
+            setCurrentUser("")
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
 
     return (
         <div>
             <Navbar
                 signInWithGoogle={() => signInWithGoogle()}
+                currentUser={currentUser}
+                logout={() => logout()}
             />
             <DateFilter/>
             <Map/>
--- daily_rainfall_react/src/components/Navbar.js
+++ daily_rainfall_react/src/components/Navbar.js
@@ -1,25 +1,31 @@
 import React from "react"
-import { Button } from "grommet"
 
 /*
     Props:
         signInWithGoogle
+        currentUser
+        Logout
 */
 
 function Navbar(props) {
     return (
         <div id="navbar-container">
             <h1>UP Rainfall Project</h1>
-            {/* If user signed in change Welcome to show username */}
-            <h2>Welcome</h2>
+            {
+                // If user signed in change Welcome to show username
+                props.currentUser ? 
+                <h2>{props.currentUser}</h2> : 
+                <h2>Welcome</h2>
+            }
             <div id="navbar-btn-container">
                 <button>About Project</button>
-                <Button 
-                    primary 
-                    label="Login"
-                    onClick={props.signInWithGoogle}
-                >
-                </Button>
+                {
+                    // If user signed in show logout button else show login button
+                    props.currentUser ? 
+                    <button onClick={props.logout}>Logout</button> :
+                    <button onClick={props.signInWithGoogle}>Login</button>
+                    
+                }
                 <button>Upload Data</button>
             </div>
         </div>
