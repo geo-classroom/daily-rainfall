760ac32c656905e1736330f0b52044e6c92b1e7d
Riley <riley.kamstra@gmail.com>
2022-06-22 16:05:51 +0200

    Dummy rainfall data added

---
+++ daily_rainfall_react/src/data.js
@@ -0,0 +1,232 @@
+const data = [
+  // Pretoria
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 20
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 18
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 19
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 19.5
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 20.5
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 21
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 18.5
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 18.75
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 19.02
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 20.2
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 19.6
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 19.8
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 20
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 17
+  },
+  {
+    latitude: -25.75,
+    longitude: 28.2431,
+    rainfallAmount: 16.55
+  },
+  // Durban
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 50
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 49
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 51
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 52
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 50.15
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 48.75
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 49.25
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 51.75
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 52.12
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 50.95
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 50.5
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 51.6
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 49.8
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 47.5
+  },
+  {
+    latitude: -29.8587,
+    longitude: 31.0218,
+    rainfallAmount: 53
+  },
+  // Cape Town
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 101
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 100
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 100.4
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 100.85
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 97
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 100.6
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 100.75
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 101
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 102.9
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 102
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 98
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 99.5
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 99
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 101
+  },
+  {
+    latitude: -33.9249,
+    longitude: 18.4241,
+    rainfallAmount: 100
+  }
+]
+
+export default data
