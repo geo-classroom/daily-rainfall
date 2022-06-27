d91dd3fa312bbffa36182040acbabf2c0c3c73c0
Riley <riley.kamstra@gmail.com>
2022-06-27 17:43:57 +0200

    Removed get location button and replaced it with a map function STILL IN PROGRESS

--- daily_rainfall_react/src/components/UserRegistrationForm.jsx
+++ daily_rainfall_react/src/components/UserRegistrationForm.jsx
@@ -1,29 +1,35 @@
 import React, { useContext, useState } from "react"
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage"
 import { UserContext } from "../App"
+import { MapContainer, Marker, TileLayer, useMapEvents } from "react-leaflet"
 
 /*
-  Props
-  handleUserRegistrationSubmit
+	Props
+  	handleUserRegistrationSubmit
 */
 const UserRegistrationForm = (props) => {
 	const user = useContext(UserContext)
 
 	/*
-    State to hold the data from the form
-  */
+    	State to hold the data from the form
+  	*/
 	const [formData, setFormData] = useState({
 		permissionToShowLocation: false,
 		latitude: "",
 		longitude: "",
 		raingaugeType: "",
-		raingaugePhoto: "",
-		addMoreData: false
+		raingaugePhoto: ""
+	})
+
+	// State to show map or form
+	const [showMapOrForm, setShowMapOrForm] = useState({
+		showMap: false,
+		showForm: true
 	})
 
 	/*
-    As the form data changes in the form the state will be updated along with it
-  */
+    	As the form data changes in the form the state will be updated along with it
+  	*/
 	const handleChange = (event) => {
 		const { name, value, type, checked } = event.target
 		setFormData((prevFormData) => {
@@ -35,8 +41,8 @@ const UserRegistrationForm = (props) => {
 	}
 
 	/*
-    Submits the form data to the App component
-  */
+    	Submits the form data to the App component
+  	*/
 	const handleSubmit = (event) => {
 		event.preventDefault()
 		/* eslint-disable react/prop-types */
@@ -44,121 +50,122 @@ const UserRegistrationForm = (props) => {
 	}
 
 	/*
-    Gets the current location of the user 
-    Sets the latitude and longitude to the state of the form so that the form fields update
-  */
-	const getLocation = () => {
-		navigator.geolocation.getCurrentPosition(success, error)
-	}
-
-	/* 
-    Succesful postion returned from getLocation 
-    Update the formData fields
-  */
-	const success = (position) => {
-		const { latitude, longitude } = position.coords
-		setFormData((prevFormData) => {
-			return {
-				...prevFormData,
-				latitude,
-				longitude
-			}
+		Hide the registration form
+		Show map
+		center map and zoom to users location
+		use a pin as a marker
+		button to submit location
+	*/
+	const getLocationMap = () => {
+		setShowMapOrForm({
+			showMap: true,
+			showForm: false
 		})
 	}
 
 	/*
-    If an error is returned from getLocation 
-    Console log the error
-  */
-	const error = (error) => {
-		console.log(error)
+		When the map loads place a marker on the map at the users location
+	*/
+	// TODO Fix up loaction getter
+	const UserLocationMarker = () => {
+		const [userLocation, setUserLocation] = useState(null)
+		const map = useMapEvents({
+			click() {
+				map.locate()
+			},
+			locationfound(event) {
+				setUserLocation(event.latlng)
+				map.flyTo(event.latlng, map.getZoom())
+			}
+		})
+
+		return userLocation === null ? null : (
+			<Marker position={userLocation}></Marker>
+		)
 	}
 
 	return (
 		<div id="form-container">
-			<form id="form" onSubmit={handleSubmit}>
-				<h1>Register</h1>
-				<label>
+			{showMapOrForm.showForm && (
+				<form id="form" onSubmit={handleSubmit}>
+					<h1>Register</h1>
+					<label>
+						<input
+							type="checkbox"
+							id="permissionToShowLocation"
+							checked={formData.permissionToShowLocation}
+							onChange={handleChange}
+							onClick={getLocationMap}
+							name="permissionToShowLocation"
+						/>
+						Permission to show Location
+					</label>
 					<input
-						type="checkbox"
-						id="permissionToShowLocation"
-						checked={formData.permissionToShowLocation}
+						type="text"
+						placeholder="Latitude"
 						onChange={handleChange}
-						name="permissionToShowLocation"
+						name="latitude"
+						value={formData.latitude}
+						disabled
 					/>
-					Permission to show Location
-				</label>
-				<input
-					type="text"
-					placeholder="Latitude"
-					onChange={handleChange}
-					name="latitude"
-					value={formData.latitude}
-					disabled
-				/>
-				<input
-					type="text"
-					placeholder="Longitude"
-					onChange={handleChange}
-					name="longitude"
-					value={formData.longitude}
-					disabled
-				/>
-				<button
-					type="button"
-					onClick={getLocation}
-					disabled={!formData.permissionToShowLocation}
-				>
-					Get Location
-				</button>
-				<select
-					id="raingaugeType"
-					value={formData.raingaugeType}
-					onChange={handleChange}
-					name="raingaugeType"
-				>
-					<option value="Option 1">Option 1</option>
-					<option value="Option 2">Option 2</option>
-					<option value="Option 3">Option 3</option>
-				</select>
-				<input
-					type="file"
-					onChange={(event) => {
-						const { files } = event.target
+					<input
+						type="text"
+						placeholder="Longitude"
+						onChange={handleChange}
+						name="longitude"
+						value={formData.longitude}
+						disabled
+					/>
+					<label>Rain gauge type</label>
+					<select
+						id="raingaugeType"
+						value={formData.raingaugeType}
+						onChange={handleChange}
+						name="raingaugeType"
+					>
+						<option default value="Manual">
+							Manual
+						</option>
+						<option value="Automatic">Automatic</option>
+					</select>
+					<input
+						type="file"
+						onChange={(event) => {
+							const { files } = event.target
 
-						// Uplaod the image to Firebase Storage
-						const storage = getStorage()
-						const storageRef = ref(storage, `${user.id}/${files[0].name}`)
-						uploadBytes(storageRef, files[0]).then((snapshot) => {
-							// Get the download link and update the state to hold the file image link
-							getDownloadURL(storageRef).then((url) => {
-								setFormData((prevFormData) => {
-									return {
-										...prevFormData,
-										raingaugePhoto: url
-									}
+							// Uplaod the image to Firebase Storage
+							const storage = getStorage()
+							const storageRef = ref(storage, `${user.id}/${files[0].name}`)
+							uploadBytes(storageRef, files[0]).then((snapshot) => {
+								// Get the download link and update the state to hold the file image link
+								getDownloadURL(storageRef).then((url) => {
+									setFormData((prevFormData) => {
+										return {
+											...prevFormData,
+											raingaugePhoto: url
+										}
+									})
 								})
 							})
-						})
-					}}
-				/>
-				<label>
-					<input
-						type="checkbox"
-						id="addMoreData"
-						checked={formData.addMoreData}
-						onChange={handleChange}
-						name="addMoreData"
+						}}
 					/>
-					Add more Data
-				</label>
-				{/* Wait for location coordinates to load before allowing user to submit */}
-				<input
-					disabled={!formData.latitude || !formData.raingaugePhoto}
-					type="submit"
-					value="submit"
-				/>
-			</form>
+					{/* Wait for location coordinates to load before allowing user to submit */}
+					<input
+						disabled={!formData.latitude || !formData.raingaugePhoto}
+						type="submit"
+						value="submit"
+					/>
+				</form>
+			)}
+			{showMapOrForm.showMap && (
+				<MapContainer center={[-28.7, 24.5]} zoom={6}>
+					<TileLayer
+						attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+						url="https://api.mapbox.com/styles/v1/riley-5/cl3shshxv000515qntejbm29o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmlsZXktNSIsImEiOiJjbDNyZzdxeTIwbTAwM2NwZnN1cG41MWkxIn0.0EmF55wuBJY-2FHaRK73kQ"
+					/>
+					<UserLocationMarker />
+				</MapContainer>
+			)}
 		</div>
 	)
 }
