cf2fdd4908cc4d894b6652ff200741325103b041
Riley <riley.kamstra@gmail.com>
2022-07-05 22:24:47 +0200

    Responsive design for the nav bar

--- daily_rainfall_react/src/components/Navbar/Navbar.jsx
+++ daily_rainfall_react/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react"
 import { UserContext } from "../../App"
+import { DropButton, Button } from "grommet"
 import "./navbar.css"
 
 /*
@@ -16,28 +17,67 @@ const Navbar = (props) => {
 
 	return (
 		<div id="navbar-container">
-			<h1>UP Rainfall Project</h1>
+			<h1 id="main-heading">UP Daily Rainfall</h1>
+			<h4 id="responsive-heading">UP Daily Rainfall</h4>
 			{
 				// If user signed in change Welcome to show username
 				<h2>{user.username ? user.username : "Welcome"}</h2>
 			}
 			<div id="navbar-btn-container">
-				<button>About Project</button>
-				{
-					// If user signed in show logout button else show login button
-					user.username ? (
-						/* eslint-disable react/prop-types */
-						<button onClick={props.logout}>Logout</button>
-					) : (
-						<button onClick={props.login}>Login</button>
-					)
-				}
-				{
-					// If user signed in enable button
-					<button disabled={!user.username} onClick={props.uploadData}>
-						Upload Data
-					</button>
-				}
+				<div id="button-menu">
+					<button>About Project</button>
+					{
+						// If user signed in show logout button else show login button
+						user.username ? (
+							/* eslint-disable react/prop-types */
+							<button onClick={props.logout}>Logout</button>
+						) : (
+							<button onClick={props.login}>Login</button>
+						)
+					}
+					{
+						// If user signed in enable button
+						<button disabled={!user.username} onClick={props.uploadData}>
+							Upload Data
+						</button>
+					}
+				</div>
+
+				<DropButton
+					id="drop-menu"
+					label="Menu"
+					dropContent={
+						<div id="drop-menu-content">
+							<Button secondary size="small">
+								About Project
+							</Button>
+							{
+								// If user signed in show logout button else show login button
+								user.username ? (
+									/* eslint-disable react/prop-types */
+									<Button secondary size="small" onClick={props.logout}>
+										Logout
+									</Button>
+								) : (
+									<Button secondary size="small" onClick={props.login}>
+										Login
+									</Button>
+								)
+							}
+							{
+								// If user signed in enable button
+								<Button
+									secondary
+									size="small"
+									disabled={!user.username}
+									onClick={props.uploadData}
+								>
+									Upload Data
+								</Button>
+							}
+						</div>
+					}
+				/>
 			</div>
 		</div>
 	)
