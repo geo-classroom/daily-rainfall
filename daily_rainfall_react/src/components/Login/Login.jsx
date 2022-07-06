cf5dcdf34eb94c923b3ca69f215c82de404883f6
Riley <riley.kamstra@gmail.com>
2022-07-06 09:30:59 +0200

    Styled login component buttons

--- daily_rainfall_react/src/components/Login/Login.jsx
+++ daily_rainfall_react/src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import "./login.css"
+import { Google, Facebook } from "@mui/icons-material"
 
 /*
     Props
@@ -13,10 +14,10 @@ const Login = (props) => {
 			<h1>Login</h1>
 			{/* eslint-disable react/prop-types */}
 			<button id="sign-in-google-btn" onClick={props.signInWithGoogle}>
-				Sign in with Google
+				<Google /> Sign in with Google
 			</button>
 			<button id="sign-in-facebook-btn" onClick={props.signInWithFacebook}>
-				Sign in with Facebook
+				<Facebook /> Sign in with Facebook
 			</button>
 		</div>
 	)
