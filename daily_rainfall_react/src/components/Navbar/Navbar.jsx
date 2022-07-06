6ff7825c04d56c00c24f04c4f0b0a3d6ca1a7c9e
Riley <riley.kamstra@gmail.com>
2022-07-06 08:48:06 +0200

    Navbar styled and is responsive

--- daily_rainfall_react/src/components/Navbar/Navbar.jsx
+++ daily_rainfall_react/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react"
 import { UserContext } from "../../App"
-import { DropButton, Button } from "grommet"
+import { Button, Menu, MenuItem } from "@mui/material"
 import "./navbar.css"
 
 /*
@@ -8,17 +8,47 @@ import "./navbar.css"
 		login
         logout
         uploadData
-        showLogin
 */
 
 const Navbar = (props) => {
 	// Get the user context
 	const user = useContext(UserContext)
 
+	/*
+		Operations needed for the menu item from MUI
+	*/
+	const [anchorEl, setAnchorEl] = React.useState(null)
+	const open = Boolean(anchorEl)
+
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget)
+	}
+
+	const handleClose = () => {
+		setAnchorEl(null)
+	}
+
+	/* eslint-disable react/prop-types */
+	const handleCloseLogout = () => {
+		setAnchorEl(null)
+		props.logout()
+	}
+
+	const handleCloseLogin = () => {
+		setAnchorEl(null)
+		props.login()
+	}
+
+	const handleCloseUploadData = () => {
+		setAnchorEl(null)
+		props.uploadData()
+	}
+
 	return (
 		<div id="navbar-container">
 			<h1 id="main-heading">UP Daily Rainfall</h1>
-			<h4 id="responsive-heading">UP Daily Rainfall</h4>
+			{/* Heading for a smaller screen  */}
+			<h3 id="responsive-heading">UP Daily Rainfall</h3>
 			{
 				// If user signed in change Welcome to show username
 				<h2>{user.username ? user.username : "Welcome"}</h2>
@@ -42,42 +72,49 @@ const Navbar = (props) => {
 						</button>
 					}
 				</div>
-
-				<DropButton
-					id="drop-menu"
-					label="Menu"
-					dropContent={
-						<div id="drop-menu-content">
-							<Button secondary size="small">
-								About Project
-							</Button>
-							{
-								// If user signed in show logout button else show login button
-								user.username ? (
-									/* eslint-disable react/prop-types */
-									<Button secondary size="small" onClick={props.logout}>
-										Logout
-									</Button>
-								) : (
-									<Button secondary size="small" onClick={props.login}>
-										Login
-									</Button>
-								)
-							}
-							{
-								// If user signed in enable button
-								<Button
-									secondary
-									size="small"
-									disabled={!user.username}
-									onClick={props.uploadData}
-								>
-									Upload Data
-								</Button>
-							}
-						</div>
-					}
-				/>
+				{/* Menu for smaller screens */}
+				<div id="drop-menu">
+					<Button
+						id="basic-button"
+						variant="contained"
+						size="medium"
+						aria-controls={open ? "basic-menu" : undefined}
+						aria-haspopup="true"
+						aria-expanded={open ? "true" : undefined}
+						onClick={handleClick}
+					>
+						Menu
+					</Button>
+					<Menu
+						id="basic-menu"
+						anchorEl={anchorEl}
+						open={open}
+						onClose={() => handleClose(null)}
+						MenuListProps={{
+							"aria-labelledby": "basic-button"
+						}}
+					>
+						<MenuItem onClick={handleClose}>About Project</MenuItem>
+						{
+							// If user signed in show logout button else show login button
+							user.username ? (
+								/* eslint-disable react/prop-types */
+								<MenuItem onClick={handleCloseLogout}>Logout</MenuItem>
+							) : (
+								<MenuItem onClick={handleCloseLogin}>Login</MenuItem>
+							)
+						}
+						{
+							// If user signed in enable button
+							<MenuItem
+								disabled={!user.username}
+								onClick={handleCloseUploadData}
+							>
+								Upload Data
+							</MenuItem>
+						}
+					</Menu>
+				</div>
 			</div>
 		</div>
 	)
