397f3378a1e0f813c327a9e9f82bdc337a1ccc97
Riley <riley.kamstra@gmail.com>
2022-07-11 17:56:14 +0200

    Updated the style of the LastUpdated component

--- daily_rainfall_react/src/components/Map/LastUpdated.jsx
+++ daily_rainfall_react/src/components/Map/LastUpdated.jsx
@@ -8,13 +8,18 @@ import L from "leaflet"
 
 const LastUpdated = (props) => {
 	/* eslint-disable react/prop-types */
+
+	/*  
+        Add a container on the map 
+        Container holds text to show when the map was last updated
+    */
 	useEffect(() => {
 		if (props.mapState) {
 			const lastUpdated = L.control({ position: "topleft" })
 
 			lastUpdated.onAdd = () => {
 				const div = L.DomUtil.create("div", "last-updated")
-				div.innerText = "Last Updated: "
+				div.innerHTML = "<h2>Last Updated: </h2>"
 				return div
 			}
 			lastUpdated.addTo(props.mapState)
