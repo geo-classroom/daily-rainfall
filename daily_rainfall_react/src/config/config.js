34a97694f964b02d7f173f33e916133f0511354f
Riley <riley.kamstra@gmail.com>
2022-10-06 15:49:53 +0200

    Added a .env file

---
+++ daily_rainfall_react/src/config/config.js
@@ -0,0 +1,11 @@
+export const config = {
+    firebaseConfig: {
+        apiKey: "AIzaSyB5z7idSIvuJinVY4R0-yY3ySU1LEJaBlY",  
+        authDomain: "daily-rainfall.firebaseapp.com",      
+        projectId: "daily-rainfall",     
+        storageBucket: "daily-rainfall.appspot.com",     
+        messagingSenderId: "916245816924",     
+        appId: "1:916245816924:web:e05bce5915f02ac965e899",  
+        measurementId: "G-3VBDW6NHG3"
+    }
+}
